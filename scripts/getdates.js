5bf5cad0bd0cb76bfde55c6b9b6685835eee0d79
hagot coriantumr viana castro <havianacastro@gmail.com>
2026-07-13 11:54:57 -0600

    h

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,4 +1,19 @@
-const currentYear = new Date().getFullYear();
-document.querySelector("#currentyear").textContent = currentYear;
-
+document.querySelector("#currentyear").textContent = new Date().getFullYear();
 document.querySelector("#lastModified").textContent = `Last Modification: ${document.lastModified}`;
+
+const temp = parseFloat(document.querySelector("#temp").textContent);
+const wind = parseFloat(document.querySelector("#wind").textContent);
+
+const calculateWindChill = (t, s) => 
+    (13.12 + 0.6215 * t - 11.37 * Math.pow(s, 0.16) + 0.3965 * t * Math.pow(s, 0.16)).toFixed(1);
+
+const displayWindChill = () => {
+    const wcSpan = document.querySelector('#wind-chill');
+    if (temp <= 10 && wind > 4.8) {
+        wcSpan.textContent = calculateWindChill(temp, wind) + " °C";
+    } else {
+        wcSpan.textContent = "N/A";
+    }
+};
+
+displayWindChill();
