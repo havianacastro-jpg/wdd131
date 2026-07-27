aa839ce3c85956d584f218f308f6d9093006bf45
hagot coriantumr viana castro <havianacastro@gmail.com>
2026-07-27 17:23:01 -0600

    h

--- project/scripts/form.js
+++ project/scripts/form.js
@@ -1,49 +1,18 @@
 const products = [
-  {
-    id: "fc-1888",
-    name: "flux capacitor",
-    averagerating: 4.5
-  },
-  {
-    id: "fc-2050",
-    name: "power laces",
-    averagerating: 4.7
-  },
-  {
-    id: "fs-1987",
-    name: "time circuits",
-    averagerating: 3.5
-  },
-  {
-    id: "ac-2000",
-    name: "low voltage reactor",
-    averagerating: 3.9
-  },
-  {
-    id: "jj-1969",
-    name: "warp equalizer",
-    averagerating: 5.0
-  }
+  { id: "fc-1888", name: "Flux Capacitor", averagerating: 4.5 },
+  { id: "power-coder", name: "Power Coder", averagerating: 4.7 },
+  { id: "ac-2093", name: "Acoustic Controller", averagerating: 3.5 },
+  { id: "jj-1969", name: "Jelly Jam", averagerating: 3.9 },
+  { id: "f-230", name: "Furnace", averagerating: 4.2 }
 ];
 
-document.addEventListener("DOMContentLoaded", () => {
-    
-    const productSelect = document.getElementById("productSelect");
-    if (productSelect) {
-        products.forEach(product => {
-            const option = document.createElement("option");
-            option.value = product.id; 
-            option.textContent = product.name; 
-            productSelect.appendChild(option);
-        });
-    }
+const productSelect = document.querySelector("#productSelect");
 
-    
-    const counterDisplay = document.getElementById("reviewsCounter");
-    if (counterDisplay) {
-        let reviewCount = Number(localStorage.getItem("reviewCount_key")) || 0;
-        reviewCount++;
-        localStorage.setItem("reviewCount_key", reviewCount);
-        counterDisplay.textContent = reviewCount;
-    }
-});
+if (productSelect) {
+  products.forEach(product => {
+    const option = document.createElement("option");
+    option.value = product.id;
+    option.textContent = product.name;
+    productSelect.appendChild(option);
+  });
+}
